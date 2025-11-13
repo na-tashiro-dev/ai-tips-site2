292ed3e41ffa85b235960aab3e1a39e7b7f3dd71
na-tashiro-dev <na-tashiro@medicmedia.com>
2025-11-13 18:05:36 +0900

    Delete public/functions/api directory

--- public/functions/api/contact.js
+++
@@ -1,9 +0,0 @@
-export async function onRequestPost(context) {
-  const env = context.env;
-  const data = await context.request.json();
-  const message = (data.message || '').toString();
-
-  await env.DB.prepare(`INSERT INTO contacts (message) VALUES (?);`).bind(message).run();
-
-  return Response.json({ ok: true });
-}
